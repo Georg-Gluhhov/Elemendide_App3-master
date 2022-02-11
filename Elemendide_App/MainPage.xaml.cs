feeaba20934f0c8c95dd1cfe13ebe8a81faac4ad
Opilane TTHK <opilane@tthk.ee>
2022-02-11 14:08:03 +0200

    Add project files.

---
+++ Elemendide_App/MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Elemendide_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            //InitializeComponent();
+
+            Button Ent_btn = new Button()
+            {
+                Text = "Entry",
+                BackgroundColor = Color.LightGreen,
+            };
+
+            Button Timer_btn = new Button()
+            {
+                Text = "Timer",
+                BackgroundColor = Color.LightGreen,
+            };
+            Button cliker = new Button()
+            {
+                Text = "Clicker",
+                BackgroundColor = Color.LightGreen,
+            };
+            Button Date_btn = new Button()
+            {
+                Text = "Date/Time",
+                BackgroundColor = Color.LightGreen,
+            };
+            Button SS_btn = new Button()
+            {
+                Text = "Stepper/Slider",
+                BackgroundColor = Color.LightGreen,
+            };
+            Button vlg_btn = new Button()
+            {
+                Text = "Valgusfoor",
+                BackgroundColor = Color.LightGreen,
+            };
+
+            StackLayout st = new StackLayout()
+            {
+                Children = { Ent_btn , Timer_btn , cliker , Date_btn , SS_btn,vlg_btn }
+            };
+
+            st.BackgroundColor = Color.AntiqueWhite;
+            Content = st;
+            Ent_btn.Clicked += Ent_btn_Clicked;
+            Timer_btn.Clicked += Timer_btn_Clicked;
+            cliker.Clicked += Cliker_Clicked;
+            Date_btn.Clicked += Date_btn_Clicked;
+            SS_btn.Clicked += SS_btn_Clicked;
+            vlg_btn.Clicked += Vlg_btn_Clicked;
+        }
+
+        private async void Vlg_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Valgusfoor());
+        }
+
+        private async void SS_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new StepperSlider_page());
+        }
+
+        private async void Date_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Date_Time());
+        }
+
+        private async void Cliker_Clicked(object sender, EventArgs e)
+        {
+             await Navigation.PushAsync(new cliker());
+        }
+
+        private async void Timer_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TimerPage());
+        }
+
+        private async void Ent_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Ent_page());
+        }
+    }
+    
+}
---
+++ Elemendide_App/Valgusfoor.xaml.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Elemendide_App
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Valgusfoor : ContentPage
+    {
+        Label lblRed,lblGreen,lblYellow;
+        Frame GreenBox, YellowBox, RedBox;
+        Button OnBtn,OffBtn;
+        bool ON_OFF = true;
+        public Valgusfoor()
+        {
+            this.BackgroundColor = Color.White;
+            lblRed = new Label()
+            {
+                TextColor = Color.White,
+                HorizontalTextAlignment=TextAlignment.Center,
+                Text ="Red"
+
+            };           
+            this.BackgroundColor = Color.White;
+            lblGreen = new Label()
+            {
+                TextColor = Color.White,
+                HorizontalTextAlignment=TextAlignment.Center,
+                Text ="Green"
+
+            };           
+            this.BackgroundColor = Color.White;
+            lblYellow = new Label()
+            {
+                TextColor = Color.White,
+                HorizontalTextAlignment=TextAlignment.Center,
+                Text ="Yellow"
+
+            };
+            OnBtn = new Button()
+            {
+                TextColor = Color.White,
+                Text = "Turn On",
+                HorizontalOptions = LayoutOptions.Start,
+    
+            };
+            OffBtn = new Button()
+            {
+                TextColor = Color.White,
+                Text = "Turn Off",
+                HorizontalOptions = LayoutOptions.Start,
+                VerticalOptions = LayoutOptions.End
+            };
+
+            GreenBox = new Frame()
+            {
+                Content= lblGreen,
+                CornerRadius = 1000,
+                WidthRequest = 300,
+                HeightRequest = 200,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                BackgroundColor = Color.Gray,
+            };
+            YellowBox = new Frame()
+            {
+                Content = lblYellow,
+                BackgroundColor = Color.Gray,
+                CornerRadius = 1000,
+                WidthRequest = 300,
+                HeightRequest = 200,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+            RedBox = new Frame()
+            {
+                Content = lblRed,
+                BackgroundColor = Color.Gray,
+                CornerRadius = 1000,
+                WidthRequest = 300,
+                HeightRequest = 200,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+            OnBtn.Clicked += OnBtn_Clicked;
+            OffBtn.Clicked += OffBtn_Clicked;
+            StackLayout st = new StackLayout { Children = { GreenBox, YellowBox ,RedBox, OnBtn, OffBtn } };
+            Content = st;
+        }
+
+        private async void OffBtn_Clicked(object sender, EventArgs e)
+        {
+            ON_OFF = false;
+        }
+
+        private async void OnBtn_Clicked(object sender, EventArgs e)
+        {
+            ON_OFF = true;
+            while (ON_OFF==true) { 
+            GreenBox.BackgroundColor = Color.Green;
+            await Task.Delay(5000);
+            GreenBox.BackgroundColor = Color.Gray;
+            await Task.Delay(100);
+            GreenBox.BackgroundColor = Color.Green;
+            await Task.Delay(100);
+            GreenBox.BackgroundColor = Color.Gray;
+            await Task.Delay(100);          
+                GreenBox.BackgroundColor = Color.Green;
+            await Task.Delay(100);
+            GreenBox.BackgroundColor = Color.Gray;
+            await Task.Delay(100);
+
+            YellowBox.BackgroundColor = Color.FromRgb(100, 100, 0);
+            await Task.Delay(2500);
+                YellowBox.BackgroundColor = Color.Gray;
+            await Task.Delay(100);
+                YellowBox.BackgroundColor = Color.FromRgb(255,255,0); 
+            await Task.Delay(100);
+                YellowBox.BackgroundColor = Color.Gray;
+            await Task.Delay(100);
+
+            RedBox.BackgroundColor = Color.FromRgb(255, 0, 0);
+            await Task.Delay(5000);
+                RedBox.BackgroundColor = Color.Gray;
+            await Task.Delay(100);
+                RedBox.BackgroundColor = Color.FromRgb(255, 0,0); 
+            await Task.Delay(100);
+                RedBox.BackgroundColor = Color.Gray;
+            await Task.Delay(100);
+                RedBox.BackgroundColor = Color.FromRgb(255, 0,0); 
+            await Task.Delay(100);
+                RedBox.BackgroundColor = Color.Gray;
+
+                YellowBox.BackgroundColor = Color.FromRgb(100, 100, 0);
+                await Task.Delay(2500);
+                YellowBox.BackgroundColor = Color.Gray;
+                await Task.Delay(100);
+                YellowBox.BackgroundColor = Color.FromRgb(255, 255, 0);
+                await Task.Delay(100);
+                YellowBox.BackgroundColor = Color.Gray;
+                await Task.Delay(100);
+
+
+            }
+        }
+    }
+}
